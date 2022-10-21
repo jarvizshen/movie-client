3dbb1294fbf39256d011b32b5d520fbcd5279fe7
jarvizz <11531813+jarvizz@user.noreply.gitee.com>
2022-10-21 14:10:15 +0800

    demo

---
+++ pages/detail/detail.vue
@@ -0,0 +1,75 @@
+<template>
+	<image :src="img" style="height: 500px;"></image>
+	<view class="movie_name">
+		<h1>片名：{{name}}</h1>
+	</view>
+	<h3>导演：</h3>
+	<view class="director">
+		<image :src="director.img" style="height: 400px;"></image>
+		<view class="director_name">
+		<p>{{director.name}}</p>
+		</view>
+	</view>
+	<h3>演员：</h3>
+	<swiper class="actors" :autoplay="false" :interval="2000" :duration="500" :circular="true"
+		indicator-active-color="000" :easing-function="true" :indicator-dots="true" style="height: 500px;">
+		<swiper-item v-for="actor in actors">
+			<view>
+				<image :src="actor.img" style="height: 400px;"></image>
+				<view class="actor_name">
+				<p>{{actor.name}}</p>
+				</view>
+			</view>
+		</swiper-item>
+	</swiper>
+	<view class="rating" v-for="rating in ratings">
+		<h3>评分：</h3>
+		<p>{{rating.title}} {{rating.ratio}}% <progress :percent="rating.ratio"></progress></p>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name: '',
+				img: '',
+				actors: [],
+				director: {},
+				ratings: []
+			}
+		},
+		onLoad(option) {
+			const movie = JSON.parse(decodeURIComponent(option.movie))
+			// console.log(movie);
+			this.actors = movie.actors
+			this.director = movie.director
+			// console.log(this.actors);
+			this.name = movie.name
+			this.img = movie.big_image
+			uni.request({
+				url: `http://localhost:8080/api/rating?movieName=${this.name}`,
+				success: (result) => {
+					if (result.statusCode == 200) {
+						// console.log(result);
+						this.ratings = result.data
+						console.log(this.ratings);
+					}
+				}
+			})
+		}
+
+	}
+</script>
+
+<style scoped>
+.movie_name{
+	text-align: center;
+}
+.director_name{
+	text-align: center;
+}
+.actor_name{
+	text-align: center;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,296 @@
+<template>
+	<view class="content">
+		<view class="top">
+			<view class="index-button">
+				<view class="icon iconfont icon-1804-liufupinggongnengtubiao_huaban1fuben23"></view>
+			</view>
+			<view class="top-bar">
+				<view class="nav-item" v-for="type in typeList" @click="clearPage(),loadMoviesByType(type.name)">
+					{{type.name}}
+				</view>
+			</view>
+			<!-- <view class="index-button">
+				<view class="icon iconfont icon-icon_search"></view>
+			</view> -->
+		</view>
+		<view class="images-wall">
+			<view class="flow wall-left">
+				<list>
+					<cell class="image-item" v-for="movie,index in movieList">
+						<view v-if="index%2==0" @click="movieDetail(movie.content)">
+							<image :src="movie.content.img" mode="widthFix" class="image"></image>
+							<view class="title">{{movie.content.name}}</view>
+							<view class="author-bar">
+								<view class="author-info">
+									<view class="author">{{movie.content.director?movie.content.director.name:""}}
+									</view>
+								</view>
+								<view class="praise-count">
+									<view class="number">
+										{{movie.content.rating}}
+									</view>
+								</view>
+							</view>
+						</view>
+					</cell>
+				</list>
+
+			</view>
+			<view class="flow wall-right">
+				<list>
+					<cell class="image-item" v-for="movie,index in movieList">
+						<view v-if="index%2!=0" @click="movieDetail(movie.content)">
+							<image :src="movie.content.img" mode="widthFix" class="image"></image>
+							<view class="title">{{movie.content.name}}</view>
+							<view class="author-bar">
+								<view class="author-info">
+									<view class="author">{{movie.content.director?movie.content.director.name:""}}
+									</view>
+								</view>
+								<view class="praise-count">
+									<view class="number">
+										{{movie.content.rating}}
+									</view>
+								</view>
+							</view>
+						</view>
+					</cell>
+				</list>
+
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				movieList: [],
+				pageIndex: 0,
+				typeList: [],
+				typeName: ''
+			}
+		},
+		onLoad() {
+			this.loadTypes()
+			this.loadMovies();
+		},
+		onPullDownRefresh() {
+			this.loadMovies();
+		},
+		onReachBottom() {
+			this.pageIndex++
+			if (this.typeName == '') {
+				this.loadMovies()
+			} else {
+				this.loadMoviesByType(this.typeName)
+			}
+		},
+		methods: {
+			movieDetail(movie) {
+				uni.navigateTo({
+					url: `/pages/detail/detail?movie=${encodeURIComponent(JSON.stringify(movie))}`
+				})
+			},
+			loadTypes() {
+				uni.request({
+					url: 'http://localhost:8080/api/movies/types',
+					success: (result) => {
+						if (result.statusCode == 200) {
+							this.typeList = result.data
+						}
+					}
+				})
+			},
+			clearPage() {
+				this.pageIndex = 0
+			},
+			loadMoviesByType(movieType) {
+				this.typeName = movieType
+				uni.request({
+					url: `http://localhost:8080/api/types?movieType=${this.typeName}&&page=${this.pageIndex}`,
+					method: 'GET',
+					success: (result) => {
+						if (result.statusCode == 200) {
+							if (result.data && result.data.content) {
+								if (this.pageIndex == 0) {
+									uni.pageScrollTo({
+										scrollTop: 0,
+										duration: 300
+									})
+									this.movieList = result.data.content
+								} else {
+									this.movieList = this.movieList.concat(result.data.content)
+								}
+								console.log(this.movieList)
+							}
+							
+						}
+					}
+				})
+			},
+			//获取影片列表 
+			loadMovies() {
+				uni.request({
+					url: "http://localhost:8080/api/movies?page=" + this.pageIndex,
+					method: 'GET',
+					success: (result) => {
+						console.log("收到的结果：", result)
+						if (result.statusCode == 200) {
+							if (result.data && result.data.content) {
+								if (this.pageIndex == 0) {
+									this.movieList = result.data.content
+								} else {
+									this.movieList = this.movieList.concat(result.data.content)
+								}
+							}
+							console.log(this.movieList)
+						}
+					},
+					complete: () => {
+						uni.stopPullDownRefresh()
+						uni.hideLoading()
+					}
+				});
+
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		padding-top: 80px;
+		padding-bottom: 50px;
+		background-color: #efefef;
+	}
+
+	.top {
+		width: 750rpx;
+		position: fixed;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		top: 0;
+		left: 0;
+		z-index: 999;
+		background-color: #fff;
+	}
+
+	.top-bar {
+		flex: 1;
+		overflow-x: scroll;
+		display: flex;
+		flex-wrap: nowrap;
+		padding: 20rpx 10rpx;
+		align-items: center;
+		justify-content: flex-start;
+		position: relative;
+	}
+
+	.index-button {
+		flex-shrink: 0;
+		width: 80rpx;
+	}
+
+	.index-button .icon {
+		font-size: 46px;
+		color: #fce71c;
+	}
+
+	.index-button .icon-icon_search {
+		color: #7A7E83;
+		font-size: 30px;
+	}
+
+	.top-bar .nav-item {
+		margin-right: 50rpx;
+		flex-shrink: 0;
+		font-size: 100%;
+		color: #a2a2a2;
+		padding: 15rpx 5rpx;
+	}
+
+	.top-bar .nav-item.actived {
+		color: #000000;
+		font-size: 105%;
+		text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.3);
+		border-bottom: 2px solid #efefef;
+	}
+
+	.images-wall {
+		display: flex;
+		width: 750rpx;
+		justify-content: space-between;
+		align-items: flex-start;
+		background: #ebebeb;
+		min-height: 100vh;
+		flex-wrap: wrap;
+	}
+
+	.images-wall .image-item {
+		width: 363rpx;
+		flex-shrink: 0;
+		border-radius: 5px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		background: #fff;
+		margin-top: 20rpx;
+		margin-left: 7rpx;
+		margin-right: 7rpx;
+	}
+
+	.images-wall .image-item .image {
+		width: 100%;
+		min-height: 60px;
+		border-radius: 5px 5px 0 0;
+	}
+
+	.images-wall .image-item .title {
+		padding: 5rpx 10rpx;
+	}
+
+	.images-wall .image-item .author-bar {
+		padding: 5rpx 10rpx;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.images-wall .image-item .author-bar .author {
+		color: #686868;
+		font-size: 70%;
+		margin-left: 20rpx;
+	}
+
+	.images-wall .image-item .author-bar .author-info {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
+	.images-wall .image-item .author-bar .author-info .avartar {
+		width: 50rpx;
+		height: 50rpx;
+	}
+
+	.images-wall .image-item .author-bar .author-info .avartar image {
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+	}
+
+	.images-wall .image-item .author-bar .praise-count {
+		display: flex;
+		align-items: center;
+		color: #686868;
+	}
+
+	.images-wall .image-item .author-bar .praise-count .number {
+		margin-left: 10rpx;
+		font-size: 90%;
+	}
+</style>
